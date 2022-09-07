8be085e83e040f20c23f679cfb071e38b0e3f49f
idjihw <redchoco06@gmail.com>
2022-09-08 00:16:37 +0900

    add react

---
+++ my-app/src/App.js
@@ -0,0 +1,37 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React, {useState} from 'react';
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+
+function App() {
+
+  const [products, setProducts] = useState([
+    { id: 'uuid-1', productName: '콜롬비아 커피1', category: '커피빈', price: 5000},
+    { id: 'uuid-2', productName: '콜롬비아 커피2', category: '커피빈', price: 5000},
+    { id: 'uuid-3', productName: '콜롬비아 커피3', category: '커피빈', price: 5000},
+  ]);
+
+  const [items, setItems] = useState([]);
+
+
+  return (
+    <div className="App">
+      <div className="row justify-content-center m-4">
+        <h1 className="text-center">Grids &amp; Circle</h1>
+      </div>
+      <div className="card">
+        <div className="row">
+          <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+            <ProductList products={products}/>
+          </div>
+          <div className="col-md-4 summary p-4">
+            <Summary/>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
